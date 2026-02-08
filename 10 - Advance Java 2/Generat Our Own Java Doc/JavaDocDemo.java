cabcad664e073d89b8066e428a50dc645a853c94
Kirit Kateshiya <kateshiyakirit96@gmail.com>
2026-02-08 11:51:00 +0530

    Add files via upload

---
+++ 10 - Advance Java 2/Generat Our Own Java Doc/JavaDocDemo.java
@@ -0,0 +1,16 @@
+/**
+ This Is A Demonstrat what JavaDocuments And How It Is A Used in the java industry
+ @author Kirit (Kirit Kateshiya)
+ @version 3.0
+ @since 2025
+ @see <a href="http://127.0.0.1:5500/myDocs1/JavaDocDemo.html" target="_blank"> Java Docs</a>
+*/
+
+public class JavaDocDemo {
+    public void add(int a, int b){
+        System.out.println("The Sum Is A: " + (a+b));
+    }
+    public static void main(String[] args) {
+        System.out.println("This Is a Main Method");
+    }
+}
---
+++ 10 - Advance Java 2/Generat Our Own Java Doc/myDocs1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"JavaDocDemo","l":"add(int, int)","url":"add(int,int)"},{"p":"<Unnamed>","c":"JavaDocDemo","l":"JavaDocDemo()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"JavaDocDemo","l":"main(String[])","url":"main(java.lang.String[])"}]
